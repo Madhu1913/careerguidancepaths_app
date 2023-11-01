7ce9c828ae911f81e9804b154217cf6bfd3fe86b
HP <madhunarayanaedu1913@gmail.com>
2023-11-01 14:32:31 +0530

    modified the code as required and removed some bugs

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:careerguidancepaths_app/Extra/Theme/ThemeProvider.dart';
 import 'package:careerguidancepaths_app/auth/auth_Page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 import 'app/account/Provider/CPIprovider.dart';
 import 'app/account/Provider/careerPointsDataProvider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp
+  ]);
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
@@ -29,6 +34,7 @@ class MyApp extends StatelessWidget {
           child: Consumer<ThemeProvider>(
             builder:(BuildContext context,value,Widget? child)=> MaterialApp(
               theme: value.themeData,
+              title: 'Career Catalyst',
               home: const AuthPage(),
               debugShowCheckedModeBanner: false,
             ),
